d18f222cb87e87ce7bacf6a0d726f7bc4ee43e2b
ff34d <urajimirudev@gmail.com>
2025-10-12 19:15:36 +0400

    feat(ui-kit): add pagination

--- src/pages/components/main/ui/MainPage.tsx
+++ src/pages/components/main/ui/MainPage.tsx
@@ -1,5 +1,4 @@
-import { Table } from "@/shared/ui/Table"
-import type { FC } from "react"
+import { type FC } from "react"
 
 export const MainPage: FC = () => {
   return (
@@ -13,27 +12,7 @@ export const MainPage: FC = () => {
         alignItems: "center",
         minHeight: "100vh",
       }}>
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Alice</td>
-            <td>30</td>
-            <td>New York</td>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>24</td>
-            <td>London</td>
-          </tr>
-        </tbody>
-      </Table>
+      Main
     </div>
   )
 }
