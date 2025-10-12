20b64a81eafa86139555e4efe00839efd2edbcdd
ff34d <urajimirudev@gmail.com>
2025-10-12 17:55:12 +0400

    feat(ui-kit): add table

--- src/pages/components/main/ui/MainPage.tsx
+++ src/pages/components/main/ui/MainPage.tsx
@@ -1,3 +1,4 @@
+import { Table } from "@/shared/ui/Table"
 import type { FC } from "react"
 
 export const MainPage: FC = () => {
@@ -12,7 +13,27 @@ export const MainPage: FC = () => {
         alignItems: "center",
         minHeight: "100vh",
       }}>
-      Main
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alice</td>
+            <td>30</td>
+            <td>New York</td>
+          </tr>
+          <tr>
+            <td>Bob</td>
+            <td>24</td>
+            <td>London</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   )
 }
---
+++ src/shared/ui/Table/ui/Table.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/shared/utils"
+import type { FC, HTMLProps, ReactNode } from "react"
+import styles from "../styles/index.module.scss"
+
+interface Props extends HTMLProps<HTMLDivElement> {
+  children: ReactNode
+}
+
+export const Table: FC<Props> = ({ children, className, ...args }) => {
+  return (
+    <div
+      {...args}
+      className={cn(styles.table__wrapper, {
+        [className as string]: className,
+      })}>
+      <table className={styles.table}>{children}</table>
+    </div>
+  )
+}
