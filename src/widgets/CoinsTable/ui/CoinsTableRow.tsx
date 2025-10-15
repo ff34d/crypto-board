0a5cbfabba38c582d99300e63b84d866cbf39828
ff34d <urajimirudev@gmail.com>
2025-10-15 19:33:29 +0400

    refactor(coins-table): add comments

--- src/widgets/CoinsTable/ui/CoinsTableRow.tsx
+++ src/widgets/CoinsTable/ui/CoinsTableRow.tsx
@@ -21,8 +21,10 @@ export const CoinsTableRow: FC<Props> = ({
 }) => {
   return (
     <tr>
+      {/* Market rank */}
       <td align="center">{market_cap_rank ?? "–"}</td>
 
+      {/* Coin name */}
       <td>
         <Box gap={0.2}>
           <img
@@ -47,6 +49,7 @@ export const CoinsTableRow: FC<Props> = ({
         </Box>
       </td>
 
+      {/* Coin price */}
       <td align="left">
         {current_price
           ? current_price < 0.01
@@ -55,6 +58,7 @@ export const CoinsTableRow: FC<Props> = ({
           : "–"}
       </td>
 
+      {/* Coin 24 percentage trending  */}
       <td align="left">
         <CoinTrendValue
           trend={price_change_percentage_24h !== null && price_change_percentage_24h > 0}>
@@ -64,8 +68,10 @@ export const CoinsTableRow: FC<Props> = ({
         </CoinTrendValue>
       </td>
 
+      {/* Market capitalize */}
       <td align="left">{market_cap ? "$" + market_cap.toLocaleString("en-US") : "–"}</td>
 
+      {/* Total volume */}
       <td align="left">
         {total_volume ? "$" + total_volume.toLocaleString("en-US") : "–"}
       </td>
